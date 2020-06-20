507b030ee60970c264f8bc186fd939b5204379c9
Xesau <aron@xesau.eu>
2020-06-20 18:26:16 +0200

    Command classes

--- Scripts/sm.interop/commands.lua
+++ Scripts/sm.interop/commands.lua
@@ -1,5 +1,6 @@
 -- @import
 local assertArg = sm.interop.util.assertArgumentType
+local getFullName = sm.interop.util.getFullName
 local mods = sm.interop.mods
 
 -- @private
@@ -9,17 +10,52 @@ local function chatError(message)
     sm.gui.chatMessage('#ff0000Error: #ffffff' .. message)
 end
 
+local function wrapNetwork(network, modName, commandName)
+    return {
+        setClientData = function() error('Not implemented') end,
+        sendToClient = function(self, player, functionName, params)
+            assertArg(2, player, 'Player')
+            assertArg(3, functionName, 'string')
+            network:sendToClient(player, 'sv_cl_commandSubFunction', {
+                modName = modName,
+                commandName = commandName,
+                functionName = functionName,
+                params = params
+            })
+        end,
+        sendToClients = function(self, functionName, params)
+            assertArg(2, functionName, 'string')
+            network:sendToClients(player, 'sv_cl_commandSubFunction', {
+                modName = modName,
+                commandName = commandName,
+                functionName = functionName,
+                params = params
+            })
+        end,
+        sendToServer = function(self, functionName, params)
+            assertArg(2, functionName, 'string')
+            network:sendToServer('sv_cl_commandSubFunction', {
+                modName = modName,
+                commandName = commandName,
+                functionName = functionName,
+                params = params
+            })
+        end
+    }
+end
+
 -- @public
 local commands = {}
 
 function commands.register(mod, commandName, handler)
     assertArg(1, mod, 'table')
     assertArg(2, commandName, 'string')
-    assertArg(3, handler, 'function')
 
     mods.assertIsValid(mod)
     commandName = commandName:lower()
-    local namespace = mod:getNamespace():lower()
+
+    local fn = getFullName(mod, commandName)
+    assert(type(handler) == 'function' or (type(handler) == 'table' and type(handler.client_onCall) == 'function'), 'Handler for '..fn..' must be function or command handler class')
 
     if commandRegistry[commandName] == nil then
         commandRegistry[commandName] = {
@@ -27,12 +63,13 @@ function commands.register(mod, commandName, handler)
         }
     end
 
+    local namespace = mod:getNamespace():lower()
     assert(commandRegistry[commandName][namespace] == nil, 'A command with this name was already registered by this mod');
     commandRegistry[commandName][namespace] = handler
     commandRegistry[commandName]['#n'] = commandRegistry[commandName]['#n'] + 1
 end
 
-function commands.call(commandName, args)
+function commands.call(commandName, args, network)
     local colonIndex = commandName:find(':', 0, true)
     local modName = nil
     if colonIndex ~= nil then
@@ -58,6 +95,7 @@ function commands.call(commandName, args)
         end
         for k,v in pairs(commandRegistry[commandName]) do
             if k ~= '#n' then
+                modName = k
                 handler = v
                 break
             end
@@ -70,7 +108,23 @@ function commands.call(commandName, args)
         end
         handler = command[modName]
     end
-    handler(args)
+    if type(handler) == 'table' then
+        handler.network = wrapNetwork(network, modName, commandName)
+        handler.client_onCall(handler, args)
+    else
+        handler(args)
+    end
+end
+
+function commands.callSubFunction(modName, commandName, functionName, params)
+    local obj = commandRegistry[commandName][modName]
+    if type(obj) ~= 'table' then
+        error('Cannot call sub function on function handler')
+    end
+    if type(obj[functionName]) ~= 'function' then
+        error('Attempting to call non-existent '.. functionName .. ' on '.. commandName .. ' Commamnd class')
+    end
+    obj[functionName](obj, params)
 end
 
 -- @export
