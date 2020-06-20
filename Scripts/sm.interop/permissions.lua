406fd4af5cb0bf946b5ed3765a83fe6a3283cff2
Xesau <aron@xesau.eu>
2020-06-20 18:26:32 +0200

    Permissions bugfixes

--- Scripts/sm.interop/permissions.lua
+++ Scripts/sm.interop/permissions.lua
@@ -1,5 +1,8 @@
-sm.interop.permissions = {}
+-- @import
+local assertArg = sm.interop.util.assertArgumentType
+local mods = sm.interop.mods
 
+-- @private
 local permissionMod = nil
 local permissions = {}
 local permissionManager = nil
@@ -23,8 +26,12 @@ local DefaultPermissionManager = {
     end,
 
     hasPermission = function(self, player, permission)
-        local level = sm.interop.permissions.getRegisteredPermissions()[permission].default
-        return self:checkPlayerLevel(player, level)
+        local permission = sm.interop.permissions.getRegisteredPermissions()[permission]
+        if permission == nil then
+            return false
+        end
+        local defaultLevel = permission.default
+        return self:checkPlayerLevel(player, defaultLevel)
     end,
 
     getPermissions = function(self, player)
@@ -46,6 +53,9 @@ local function getPermissionManager()
     end
     return permissionManager
 end
+
+-- @public
+sm.interop.permissions = {}
 
 --- Registers a permission node.
 -- A permission node is an 'option' that can be turned on or off for a specific player.
@@ -77,18 +87,22 @@ end
 --- Checks whether a player is the host.
 -- @return `boolean`
 function sm.interop.permissions.isHost(player)
+    assertArg(1, player, 'Player')
     return player.id == 1
 end
 
 --- Checks if the user has a certain permission
 -- @return True if the player has the permission, false if not
 function sm.interop.permissions.hasPermission(player, permission)
-    return getPermissionManager():hasPermission(player, permission)
+    assertArg(1, player, 'Player')
+    assertArg(2, permission, 'string')
+    return getPermissionManager():hasPermission(player, permission:lower())
 end
 
 --- Gets all permissions a player has
 -- @return table {string...}
 function sm.interop.permissions.getPermissions(player)
+    assertArg(1, player, 'Player')
     return getPermissionManager():getPermissions(player)
 end
 
@@ -99,7 +113,11 @@ end
 --   - hasPermission(self, player, permission): boolean
 --   - getPermissions(self, player): table {string...}
 function sm.interop.permissions.setPermissionManager(mod, manager)
+    mods.assertValid(mod)
+    assertArg(2, manager, 'table')
     assert(permissionManager == nil, 'Another permission manager was already set by ' .. permissionMod:getName())
+    assert(manager.hasPermission ~= nil, 'PermissionManager is missing hasPermission(player, permission)')
+    assert(manager.getPermissions ~= nil, 'PermissionManager is missing getPermissions(players)')
     permissionMod = mod
     permissionManager = manager
 end
