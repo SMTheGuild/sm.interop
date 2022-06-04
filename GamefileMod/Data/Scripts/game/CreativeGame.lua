3e37b7c9013b7d03e1c417ed0e3a75b8add43987
TechnologicNick <technologicnick@gmail.com>
2022-06-04 20:07:36 +0200

    Bump gamefileModVersion to 5

--- GamefileMod/Data/Scripts/game/CreativeGame.lua
+++ GamefileMod/Data/Scripts/game/CreativeGame.lua
@@ -234,5 +234,5 @@ CreativeCustomGame.worldScriptClass = "CreativeCustomWorld";
 CreativeTerrainGame = class( CreativeGame )
 CreativeTerrainGame.worldScriptFilename = "$GAME_DATA/Scripts/game/worlds/CreativeTerrainWorld.lua";
 CreativeTerrainGame.worldScriptClass = "CreativeTerrainWorld";
-sm.interopGamefileModVersion = 4
+sm.interopGamefileModVersion = 5
 dofile '$CONTENT_e94ac99f-393e-4816-abe3-353435a1edf4/Scripts/Overrides/CreativeGame.lua'
--- GamefileMod_src/Scripts/game/CreativeGame.lua
+++ GamefileMod_src/Scripts/game/CreativeGame.lua
@@ -1,2 +1,2 @@
-sm.interopGamefileModVersion = 4
+sm.interopGamefileModVersion = 5
 dofile '$CONTENT_e94ac99f-393e-4816-abe3-353435a1edf4/Scripts/Overrides/CreativeGame.lua'
